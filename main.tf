340da95c213391f6615f7e46c057a84910fd24ff
MOHAMMED-ho <mohammed.shareef@digital.homeoffice.gov.uk>
2024-10-08 14:46:30 +0100

    defining the base wafv2 terraform module
    adding the git workflows and removing waf rules
    
    adding missing bracket

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_wafv2_web_acl" "this" {
+  name        = var.waf_acl_name
+  description = var.waf_acl_description
+  scope       = var.scope
+
+  default_action {
+    allow {}
+  }
+}
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "webACL"
+    sampled_requests_enabled   = true
+  }
+
+resource "aws_wafv2_web_acl_association" "this" {
+  resource_arn = var.resource_arn
+  web_acl_arn  = aws_wafv2_web_acl.this.arn
+}
