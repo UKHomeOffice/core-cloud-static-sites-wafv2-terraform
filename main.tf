4fd6bf3a5cccbe06b96bf5c1d6a5c28b6981857b
ishaikh-ho <iqbal.shaikh2@digital.homeoffice.gov.uk>
2024-12-19 17:00:04 +0000

    Adding AWSCommonRules to WAF (#6)

--- main.tf
+++ main.tf
@@ -1,10 +1,34 @@
 resource "aws_wafv2_web_acl" "this" {
   name        = var.waf_acl_name
   description = var.waf_acl_description
+  provider    = aws.us-east-1
   scope       = var.scope
+  tags        = var.tags
 
   default_action {
     allow {}
+  }
+
+  rule {
+    name     = "AWSManaged-CommonRule"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "static-site-${var.tenant_vars.product}-${var.tenant_vars.component}-AWSManagedCommonRuleSet-metric"
+      sampled_requests_enabled   = false
+    }
   }
 
   visibility_config {
